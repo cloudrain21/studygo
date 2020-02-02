89096f43549795182b87b2a44df45f8fa599622e
cloudrain21 <cloudrain21@gmail.com>
2020-02-03 05:49:49 +0900

    using slice and map

---
+++ slice-exercise/map1.go
@@ -0,0 +1,108 @@
+package main
+
+import "fmt"
+
+func make_map() {
+	fmt.Println("make_map()")
+	dict := make(map[string]int)
+	dict1 := map[string]string{"Red": "#da1337", "Orange": "#e95a22"}
+	dict2 := map[int][]string{}
+
+	dict["R"] = 11
+	dict["L"] = 22
+
+	for index, value := range dict {
+		fmt.Println(index, value)
+	}
+
+	for index, value := range dict1 {
+		fmt.Println(index, value)
+	}
+
+	t := []string{"AA", "AAA"}
+	dict2[1] = t
+	t = []string{"BB", "BBB"}
+	dict2[2] = t
+
+	for index, value := range dict2 {
+		fmt.Println(index, value)
+	}
+
+	dict2[1] = append(dict2[1], "AAAA")
+	dict2[2] = append(dict2[2], "BBBB")
+
+	for index, value := range dict2 {
+		fmt.Println(index, value)
+	}
+}
+
+func nil_map() {
+	fmt.Println("nil_map()")
+	var colors map[string]int
+	// colors["Red"] = "#da1337"    // runtime error (nil map 에 key/value 저장 안됨)
+
+	m := make(map[string]int)
+	m["Red"] = 11
+	m["Blue"] = 22
+
+	colors = m
+	for key, value := range colors {
+		fmt.Println(key, value)
+	}
+}
+
+func select_map() {
+	fmt.Println("select_map()")
+	colors := make(map[string]int)
+	colors["Red"] = 11
+	colors["Blue"] = 22
+
+	value, exists := colors["Blue"]
+	if exists {
+		fmt.Println(value)
+	}
+}
+
+func check_exist() {
+	fmt.Println("check_exist()")
+	colors := make(map[string]int)
+	colors["Red"] = 11
+	colors["Blue"] = 22
+
+	value := colors["Red"]
+	if value != 0 {
+		fmt.Println("Red exists")
+	}
+
+	value1, exists := colors["Blue"]
+	fmt.Println(value1, exists)
+
+	value1, exists = colors["Black"]
+	fmt.Println(value1, exists)
+}
+
+func delete_elem() {
+	fmt.Println("delete_elem()")
+
+	colors := make(map[string]int)
+	colors["Red"] = 11
+	colors["Blue"] = 22
+
+	for key, value := range colors {
+		fmt.Println(key, value)
+	}
+
+	delete(colors, "Blue")
+
+	for key, value := range colors {
+		fmt.Println(key, value)
+	}
+}
+
+func main() {
+	make_map()
+	nil_map()
+	select_map()
+	check_exist()
+	delete_elem()
+}
--- slice-exercise/slice1.go
+++ slice-exercise/slice1.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+func make_empty_slice() {
+	fmt.Println("make_empty_slice()")
+	slice := make([]int, 0)
+	slice1 := []int{}
+
+	for index, value := range slice {
+		fmt.Println(index, value)
+	}
+	for index, value := range slice1 {
+		fmt.Println(index, value)
+	}
+}
 
 func chg_slice_index_value() {
 	fmt.Println("chg_slice_index_value()")
@@ -32,8 +48,91 @@ func append_slice() {
 	fmt.Println(slice)
 }
 
+func append_capacity() {
+	fmt.Println("append_capacity()")
+	slice := []int{10, 20, 30, 40}
+	newSlice := append(slice, 50)
+	fmt.Println(slice)
+	fmt.Println(len(slice), cap(slice))
+	fmt.Println(len(newSlice), cap(newSlice))
+	fmt.Println(newSlice)
+	newSlice[3] = 200
+	fmt.Println(slice)
+	fmt.Println(newSlice)
+}
+
+func append_slice2() {
+	fmt.Println("append_slice2()")
+	source := []string{"Apple", "Orange", "Plum", "Banana", "Grape"}
+	slice := source[2:3:3]
+	slice = append(slice, "Kiwi")
+	fmt.Println(source)
+	fmt.Println(slice)
+}
+
+func append_func() {
+	fmt.Println("append_func()")
+	s1 := []int{1, 2}
+	s2 := []int{3, 4}
+
+	fmt.Printf("%v\n", append(s1, s2...))
+	s3 := append(s1, 3, 4, 5, 6, 7)
+	fmt.Println(s3)
+}
+
+func iterage_slice() {
+	fmt.Println("iterate_slice()")
+	slice := []int{10, 20, 30, 40}
+	for index, value := range slice {
+		fmt.Printf("index: %d value: %d\n", index, value)
+	}
+
+	slice1 := []int{1, 2, 3, 4}
+	for _, value := range slice1 {
+		fmt.Printf("value : %d\n", value)
+	}
+}
+
+func multiple_division_slice() {
+	fmt.Println("multiple_division_slice()")
+	slice := [][]int{{10}, {100, 200}}
+	for index, value := range slice {
+		fmt.Printf("%d : %s\n", index, reflect.TypeOf(value))
+		fmt.Print(" ")
+		fmt.Println(value)
+	}
+	slice[0] = append(slice[0], 20)
+	for index, value := range slice {
+		fmt.Printf("%d : %s\n", index, reflect.TypeOf(value))
+		fmt.Print(" ")
+		fmt.Println(value)
+	}
+}
+
+func foo() []int {
+	slice := make([]int, 10)
+	slice[2] = 100
+	return slice
+}
+
+func function_slice() {
+	fmt.Println("start function_slice()")
+	slice := foo()
+	for index, value := range slice {
+		fmt.Println(index, value)
+	}
+	fmt.Println("end function_slice()")
+}
+
 func main() {
+	make_empty_slice()
 	chg_slice_index_value()
 	//index_out_of_range()
 	append_slice()
+	append_capacity()
+	append_slice2()
+	append_func()
+	iterage_slice()
+	multiple_division_slice()
+	function_slice()
 }
