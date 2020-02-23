a354cd3d7f22d45bb9d99bedeb00cd1e50d85e98
cloudrain21 <cloudrain21@gmail.com>
2020-02-23 23:03:31 +0900

    empty interface

--- type_system/type_assertion.go
+++ type_system/type_assertion.go
@@ -1,12 +1,20 @@
+// 참고 : https://tour.golang.org/methods/15
 package main
 
 import "fmt"
 
 func main() {
-	var i interface{} = 1
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
 	s, ok := i.(string)
 	fmt.Println(s, ok)
 
-	ss, ok := i.(int)
-	fmt.Println(ss, ok)
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64) // panic
+	fmt.Println(f)
 }
